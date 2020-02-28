4f2326d9297c706a4b0282a86a2cfe320c487135
Hunter Willoughby <hunterwilloughby@Hunters-MacBook-Pro.local>
2020-02-27 20:15:23 -0800

    Commit

---
+++ Coursera-Capstone.py
@@ -0,0 +1,30 @@
+print ('Hello Github!')
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+#Week 3
+
+#Import Libraries
+import numpy as np
+
+import pandas as pd
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
+
+import json
+
+from geopy.geocoders import Nominatim
+
+import requests
+from bs4 import BeautifulSoup
+
+from pandas.io.json import json_normalize
+
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+
+from sklearn.cluster import KMeans
+
+import folium
+print("Libraries loaded")
