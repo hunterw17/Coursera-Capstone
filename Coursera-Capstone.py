838341b769483b0629348218aa939a46858f72dd
Hunter Willoughby <hunterwilloughby@Hunters-MacBook-Pro.local>
2020-02-29 10:15:24 -0800

    Week 3

--- Coursera-Capstone.py
+++ Coursera-Capstone.py
@@ -28,3 +28,233 @@ from sklearn.cluster import KMeans
 
 import folium
 print("Libraries loaded")
+
+
+#Scrap Data from Wikipedia page into Dataframe
+data = requests.get('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M').text
+soup = BeautifulSoup(data, 'html.parser')
+postalCodeList = []
+boroughList = []
+neighborhoodList = []
+
+for row in soup.find('table').find_all('tr'):
+    cells = row.find_all('td')
+    if(len(cells) > 0):
+        postalCodeList.append(cells[0].text)
+        boroughList.append(cells[1].text)
+        neighborhoodList.append(cells[2].text.rstrip('\n'))
+
+toronto_df = pd.DataFrame({"PostalCode": postalCodeList,
+                           "Borough": boroughList,
+                           "Neighborhood": neighborhoodList})
+
+toronto_df.head()
+
+#Drop unassigned cells
+toronto_df_dropna = toronto_df[toronto_df.Borough != "Not assigned"].reset_index(drop=True)
+toronto_df_dropna.head()
+
+#Group Neighborhods in same borough
+toronto_df_grouped = toronto_df_dropna.groupby(["PostalCode", "Borough"], as_index=False).agg(lambda x: ", ".join(x))
+toronto_df_grouped.head()
+
+#Mark unassigned neighborhoods same as borough
+for index, row in toronto_df_grouped.iterrows():
+    if row["Neighborhood"] == "Not assigned":
+        row["Neighborhood"] = row["Borough"]
+toronto_df_grouped.head()
+
+#Check boroughs
+column_names = ["PostalCode", "Borough", "Neighborhood"]
+test_df = pd.DataFrame(columns=column_names)
+test_list = ["M5G", "M2H", "M4B", "M1J", "M4G", "M4M", "M1R", "M9V", "M9L", "M5V", "M1B", "M5A"]
+for postcode in test_list:
+    test_df = test_df.append(toronto_df_grouped[toronto_df_grouped["PostalCode"]==postcode], ignore_index=True)
+test_df
+
+#Print Number of Rows in cleaned DataFrame
+toronto_df_grouped.shape
+
+#Load Coordinates from CSV File
+coordinates = pd.read_csv("Geospatial_Coordinates.csv")
+coordinates.head()
+coordinates.rename(columns={"Postal Code": "PostalCode"}, inplace=True)
+coordinates.head()
+
+#Merge Tables
+toronto_df_new = toronto_df_grouped.merge(coordinates, on="PostalCode", how="left")
+toronto_df_new.head()
+
+#Check new Table
+column_names = ["PostalCode", "Borough", "Neighborhood", "Latitude", "Longitude"]
+test_df = pd.DataFrame(columns=column_names)
+test_list = ["M5G", "M2H", "M4B", "M1J", "M4G", "M4M", "M1R", "M9V", "M9L", "M5V", "M1B", "M5A"]
+for postcode in test_list:
+    test_df = test_df.append(toronto_df_new[toronto_df_new["PostalCode"]==postcode], ignore_index=True)
+test_df
+
+#Find latlong of toronto
+address = 'Toronto'
+geolocator = Nominatim(user_agent="my-application")
+location = geolocator.geocode(address)
+latitude = location.latitude
+longitude = location.longitude
+print('The geograpical coordinate of Toronto are {}, {}.'.format(latitude, longitude))
+
+#Create Map of toronto
+map_toronto = folium.Map(location=[latitude, longitude], zoom_start=10)
+for lat, lng, borough, neighborhood in zip(toronto_df_new['Latitude'], toronto_df_new['Longitude'], toronto_df_new['Borough'], toronto_df_new['Neighborhood']):
+    label = '{}, {}'.format(neighborhood, borough)
+    label = folium.Popup(label, parse_html=True)
+    folium.CircleMarker(
+        [lat, lng],
+        radius=5,
+        popup=label,
+        color='blue',
+        fill=True,
+        fill_color='#3186cc',
+        fill_opacity=0.7).add_to(map_toronto)
+map_toronto
+
+#Filter Toronto boroughs
+borough_names = list(toronto_df_new.Borough.unique())
+borough_with_toronto = []
+for x in borough_names:
+    if "toronto" in x.lower():
+        borough_with_toronto.append(x)
+borough_with_toronto
+
+toronto_df_new = toronto_df_new[toronto_df_new['Borough'].isin(borough_with_toronto)].reset_index(drop=True)
+print(toronto_df_new.shape)
+toronto_df_new.head()
+
+map_toronto = folium.Map(location=[latitude, longitude], zoom_start=10)
+for lat, lng, borough, neighborhood in zip(toronto_df_new['Latitude'], toronto_df_new['Longitude'], toronto_df_new['Borough'], toronto_df_new['Neighborhood']):
+    label = '{}, {}'.format(neighborhood, borough)
+    label = folium.Popup(label, parse_html=True)
+    folium.CircleMarker(
+        [lat, lng],
+        radius=5,
+        popup=label,
+        color='blue',
+        fill=True,
+        fill_color='#3186cc',
+        fill_opacity=0.7).add_to(map_toronto)
+map_toronto
+
+#Foursquare API
+CLIENT_ID = 'your Foursquare ID'
+CLIENT_SECRET = 'your Foursquare Secret'
+VERSION = '20180605'
+print('Your credentails:')
+print('CLIENT_ID: ' + CLIENT_ID)
+print('CLIENT_SECRET:' + CLIENT_SECRET)
+
+radius = 500
+LIMIT = 100
+venues = []
+for lat, long, post, borough, neighborhood in zip(toronto_df_new['Latitude'], toronto_df_new['Longitude'], toronto_df_new['PostalCode'], toronto_df_new['Borough'], toronto_df_new['Neighborhood']):
+    url = "https://api.foursquare.com/v2/venues/explore?client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}".format(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        VERSION,
+        lat,
+        long,
+        radius,
+        LIMIT)
+    results = requests.get(url).json()["response"]['groups'][0]['items']
+    for venue in results:
+        venues.append((
+            post,
+            borough,
+            neighborhood,
+            lat,
+            long,
+            venue['venue']['name'],
+            venue['venue']['location']['lat'],
+            venue['venue']['location']['lng'],
+            venue['venue']['categories'][0]['name']))
+venues_df = pd.DataFrame(venues)
+venues_df.columns = ['PostalCode', 'Borough', 'Neighborhood', 'BoroughLatitude', 'BoroughLongitude', 'VenueName', 'VenueLatitude', 'VenueLongitude', 'VenueCategory']
+print(venues_df.shape)
+venues_df.head()
+venues_df.groupby(["PostalCode", "Borough", "Neighborhood"]).count()
+print('There are {} uniques categories.'.format(len(venues_df['VenueCategory'].unique())))
+venues_df['VenueCategory'].unique()[:50]
+
+#Analyze Areas
+toronto_onehot = pd.get_dummies(venues_df[['VenueCategory']], prefix="", prefix_sep="")
+toronto_onehot['PostalCode'] = venues_df['PostalCode']
+toronto_onehot['Borough'] = venues_df['Borough']
+toronto_onehot['Neighborhoods'] = venues_df['Neighborhood']
+fixed_columns = list(toronto_onehot.columns[-3:]) + list(toronto_onehot.columns[:-3])
+toronto_onehot = toronto_onehot[fixed_columns]
+print(toronto_onehot.shape)
+toronto_onehot.head()
+
+toronto_grouped = toronto_onehot.groupby(["PostalCode", "Borough", "Neighborhoods"]).mean().reset_index()
+print(toronto_grouped.shape)
+toronto_grouped
+
+num_top_venues = 10
+indicators = ['st', 'nd', 'rd']
+areaColumns = ['PostalCode', 'Borough', 'Neighborhoods']
+freqColumns = []
+for ind in np.arange(num_top_venues):
+    try:
+        freqColumns.append('{}{} Most Common Venue'.format(ind+1, indicators[ind]))
+    except:
+        freqColumns.append('{}th Most Common Venue'.format(ind+1))
+columns = areaColumns+freqColumns
+
+neighborhoods_venues_sorted = pd.DataFrame(columns=columns)
+neighborhoods_venues_sorted['PostalCode'] = toronto_grouped['PostalCode']
+neighborhoods_venues_sorted['Borough'] = toronto_grouped['Borough']
+neighborhoods_venues_sorted['Neighborhoods'] = toronto_grouped['Neighborhoods']
+for ind in np.arange(toronto_grouped.shape[0]):
+    row_categories = toronto_grouped.iloc[ind, :].iloc[3:]
+    row_categories_sorted = row_categories.sort_values(ascending=False)
+    neighborhoods_venues_sorted.iloc[ind, 3:] = row_categories_sorted.index.values[0:num_top_venues]
+print(neighborhoods_venues_sorted.shape)
+neighborhoods_venues_sorted
+
+#Clusters
+kclusters = 5
+toronto_grouped_clustering = toronto_grouped.drop(["PostalCode", "Borough", "Neighborhoods"], 1)
+kmeans = KMeans(n_clusters=kclusters, random_state=0).fit(toronto_grouped_clustering)
+kmeans.labels_[0:10]
+toronto_merged = toronto_df_new.copy()
+toronto_merged["Cluster Labels"] = kmeans.labels_
+toronto_merged = toronto_merged.join(neighborhoods_venues_sorted.drop(["Borough", "Neighborhoods"], 1).set_index("PostalCode"), on="PostalCode")
+print(toronto_merged.shape)
+toronto_merged.head()
+
+print(toronto_merged.shape)
+toronto_merged.sort_values(["Cluster Labels"], inplace=True)
+toronto_merged
+
+#DataViz Clusters
+map_clusters = folium.Map(location=[latitude, longitude], zoom_start=11)
+x = np.arange(kclusters)
+ys = [i+x+(i*x)**2 for i in range(kclusters)]
+colors_array = cm.rainbow(np.linspace(0, 1, len(ys)))
+rainbow = [colors.rgb2hex(i) for i in colors_array]
+markers_colors = []
+for lat, lon, post, bor, poi, cluster in zip(toronto_merged['Latitude'], toronto_merged['Longitude'], toronto_merged['PostalCode'], toronto_merged['Borough'], toronto_merged['Neighborhood'], toronto_merged['Cluster Labels']):
+    label = folium.Popup('{} ({}): {} - Cluster {}'.format(bor, post, poi, cluster), parse_html=True)
+    folium.CircleMarker(
+        [lat, lon],
+        radius=5,
+        popup=label,
+        color=rainbow[cluster-1],
+        fill=True,
+        fill_color=rainbow[cluster-1],
+        fill_opacity=0.7).add_to(map_clusters)
+map_clusters
+
+#Evaluate Results
+toronto_merged.loc[toronto_merged['Cluster Labels'] == 0, toronto_merged.columns[[1] + list(range(5, toronto_merged.shape[1]))]]
+toronto_merged.loc[toronto_merged['Cluster Labels'] == 1, toronto_merged.columns[[1] + list(range(5, toronto_merged.shape[1]))]]
+toronto_merged.loc[toronto_merged['Cluster Labels'] == 2, toronto_merged.columns[[1] + list(range(5, toronto_merged.shape[1]))]]
+toronto_merged.loc[toronto_merged['Cluster Labels'] == 3, toronto_merged.columns[[1] + list(range(5, toronto_merged.shape[1]))]]
+toronto_merged.loc[toronto_merged['Cluster Labels'] == 4, toronto_merged.columns[[1] + list(range(5, toronto_merged.shape[1]))]]
